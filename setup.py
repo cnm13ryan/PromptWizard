79f1ed54c2d35a3def3858c7375b8908f3c9b3b8
cnm13ryan <cnm13ryan@cnm13ryans-MacBook-Pro.local>
2025-02-11 19:52:41 +0000

    chore(deps): update llama-index-core and llama-index from 0.11.10 to 0.12.04

--- setup.py
+++ setup.py
@@ -31,8 +31,8 @@ INSTALL_REQUIRES = [
     "azure-search-documents",
     "pyyaml~=6.0.1",
     "pyarrow==15.0.2",
-    "llama-index==0.11.10",
-    "llama-index-core==0.11.10",
+    "llama-index==0.12.04",
+    "llama-index-core==0.12.04",
     "llama-index-llms-ollama==0.5.2",
     "python-dotenv"
 ]
